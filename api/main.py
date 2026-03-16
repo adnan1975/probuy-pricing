3ce87fdcd73525edb167962ddb0bf32318443a4f
Adnan Rana <adnan_rana@pattisonfoodgroup.com>
2026-03-15 21:03:50 -0700

    fix cors

--- api/main.py
+++ api/main.py
@@ -8,7 +8,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://probuy-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
